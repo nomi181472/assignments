3a82773bba7c2c4c4a38627cec89e012787953b7
nomi181472 <nomansoomro52@gmail.com>
2021-03-07 21:25:04 +0500

    sd

---
+++ k173652_Q2/ParseData.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace k173652_Q2
+{
+    class ParseData
+    {
+        private List<string> Categories { get; set; }
+        ParseData()
+        {
+            Categories = new List<string>();
+        }
+
+
+    }
+}
